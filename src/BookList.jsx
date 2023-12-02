5218035364afbedea1e0f338e1244026cf4ad124
sanket <sanket.panda@viacom18.com>
2023-12-02 17:13:55 +0530

    Mount: The Mounting of BookList Component

--- src/BookList.jsx
+++ src/BookList.jsx
@@ -1,34 +1,24 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./BookList.css";
 import { produce } from "immer";
 import BooksListItem from "./BooksListItem";
 
-const initialBooks = [
-  {
-    id: 1,
-    title: "JavaScript-The Comprehensive Guide",
-    author: "Philip Ackermann",
-    isbn: "978-3836286299",
-    rating: 5,
-  },
-  {
-    id: 2,
-    title: "Clean Code",
-    author: "Reobert Martin",
-    isbn: "978-0132350884",
-    rating: 4,
-  },
-  {
-    id: 3,
-    title: "Design Patterns",
-    author: "Erich Gamma",
-    isbn: "978-0201633610",
-    rating: 5,
-  },
-];
-
 const BookList = () => {
-  const [books, setBooks] = useState(initialBooks);
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setBooks([
+        {
+          id: 1,
+          title: "JavaScript-The Comprehensive Guide",
+          author: "Philip Ackermann",
+          isbn: "978-3836286299",
+          rating: 5,
+        },
+      ]);
+    }, 2000);
+  }, []);
 
   const handleRate = (id, rating) => {
     setBooks((prevState) => {
